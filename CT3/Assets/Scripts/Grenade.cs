ab3c0eb185e6d24182888047f773f8604099cb21
L34TH3R-J4CK3T <asaph.net@gmail.com>
2020-08-08 13:29:37 -0300

    init

---
+++ CT3/Assets/Scripts/Grenade.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+
+    public float delay = 3f; // tempo para granada explodir
+    public float radius = 5f;
+    public float force = 700f;
+    
+
+    float countdown; // contagem regressiva
+    bool hasExploded = false; // se a agranada ja explodiu
+    
+
+    public GameObject explosionEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f && hasExploded == false)
+        {
+            //BOOM
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    void Explode()
+    {
+        Debug.Log("BOOM");
+
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach(Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+        }
+
+        Destroy(gameObject);
+
+    }
+}
